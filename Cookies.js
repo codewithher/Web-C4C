e33e3e74d97f0c1bdc30627da4d62a1b973cba6c
mailelucks <maile.lucks@gmail.com>
2023-04-27 21:03:21 +0000

    Updating to latest

---
+++ Cookies.js
@@ -0,0 +1,10 @@
+function showSamoas() {
+  var x = document.getElementById("samoasInfo");
+    x.style.visibility = "visible";
+}
+
+function hideSamoas() {
+  var x = document.getElementById("samoasInfo");
+    x.style.visibility = "hidden";
+}
+
--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('button')
-
-const toggleMessage = () => {
-  document.getElementById('message').classList.toggle('hide')
-}
-button.addEventListener('click', toggleMessage)
