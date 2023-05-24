6a88f0660cc826ae9d8e2808994d852d22b89af8
anchalv00 <98239195+anchalv00@users.noreply.github.com>
2023-05-24 06:55:48 +0000

    finished menu bar

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const mobileMenuTrigger = document.getElementById("mobile-menu-trigger");
-const mobileMenu = document.getElementById("header-nav");
-
+/*
 const toggleMobileMenu = () => {
   mobileMenu.classList.toggle("open");
 };
+*/
+
+let toggleMenu = function(){
+  let mobileMenu = document.getElementById("header-nav");
+  let mobileUL = document.getElementById("header-ul");
+  let mobileLink = document.querySelectorAll(".header-menu-link");
+
+  mobileMenu.classList.toggle("open-nav");
+  mobileUL.classList.toggle("open-ul");
+
+  for(let i = 0; i < mobileLink.length; i++){
+      mobileLink[i].classList.toggle("open-link");
+  }
+}
 
 mobileMenuTrigger.addEventListener("click", toggleMobileMenu);
