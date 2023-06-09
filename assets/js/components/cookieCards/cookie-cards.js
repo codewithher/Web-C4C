67be4069613a125174fc6a964c76322a8d0354ef
mailelucks <maile.lucks@gmail.com>
2023-06-09 11:37:39 +0000

    file clean up and core page styling

--- assets/js/components/cookieCards/cookie-cards.js
+++ assets/js/components/cookieCards/cookie-cards.js
@@ -18,32 +18,31 @@ class CookieCards extends HTMLElement {
     this.render();
   }
 
-
-  // [EXERCISE] What should we do here to loop through our cookie data array to output the markup? 
-
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-  
-  // HTML MARKUP
-  // Place this code inside of this.innerHTML, nested inside of the .map function after reviewing the Mozilla article.
-  
-  // <li class="cookie-card">
-  //   <div class="card-front">
-  //       <img class="cookie-image" src="assets/images/cookies/${cookie.image}.png" alt="${cookie.name}">
-  //       <p class="cookie-name">${cookie.name}</p>
-  //       <button class="cookie-front-cta">Learn More</button>
-  //   </div>
-  //   <div class="cookie-back">
-  //     <p class="cookie-description">${cookie.description}</p> 
-  //     <button class="cookie-back-cta" />
-  //   </div>
-  // </li>
-  
   render() {
-    this.innerHTML = ``;
-  }
-
-  // [EXERCISE] Using the CSS classes defined in the markup, create the styling for each element in cookies.css
-
+    this.innerHTML =
+      `<ul class="cookie-list">
+        ${this.cookies.map(cookie =>
+        `
+          <li class="cookie-card">
+            <div class="cookie-card-front">
+                <div class="cookie-image-container">
+                  <img class="cookie-image" src="assets/images/cookies/${cookie.image}.png" alt="${cookie.name}">
+                </div>
+                  <h3 class="cookie-name">
+                    ${cookie.name}
+                  </h3>
+                <div class="cookie-card-front-cta-container">
+                  <button class="cookie-front-cta">See Details</button>
+                </div>
+            </div>
+            <div class="cookie-card-back">
+                <p class="cookie-description">${cookie.description}</p> 
+              <button class="cookie-back-cta"><img src="closeBtn.png" alt="Close Button"></button>
+            </div>
+          </li>
+        `).join('')}
+      </ul>`
+  };
 }
 
 if ('customElements' in window) {
