64305808762299b3683b7bba4af6183f8d36a78b
ceciliatruong <114256675+ceciliatruong@users.noreply.github.com>
2023-11-08 18:55:12 -0800

    created cookie-month.json & refactored js code

--- assets/js/components/promoBanner/promo-banner.js
+++ assets/js/components/promoBanner/promo-banner.js
@@ -1,81 +1,17 @@
-const chooseCookieByMonth = () => {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const month = months[new Date().getMonth()];
-    console.log(month);
-    let cookieData = {
-        name: '',
-        image: '',
-        description: ''
-    }
-    switch(month) {
-        case 'January':
-            cookieData.name="S'mores";
-            cookieData.image="smores";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        case 'February':
-            cookieData.name="Adventurefuls";
-            cookieData.image="adventurefuls";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        case 'March':
-            cookieData.name="Do-si-dos";
-            cookieData.image="dosidos";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        case 'April':
-            cookieData.name="Thin Mints";
-            cookieData.image="thinmints";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        case 'May':
-            cookieData.name="Tagalongs";
-            cookieData.image="tagalongs";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        case 'June':
-            cookieData.name="Trefoils";
-            cookieData.image="trefoils";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        case 'July':
-            cookieData.name="Lemon-ups";
-            cookieData.image="lemonups";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        case 'August':
-            cookieData.name="Raspberry Rallys";
-            cookieData.image="raspberyrally";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        case 'September':
-            cookieData.name="Toffeetastic";
-            cookieData.image="toffetastic";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        case 'October':
-            cookieData.name="Samoas";
-            cookieData.image="samoas";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        case 'November':
-            cookieData.name="Adventurefuls";
-            cookieData.image="adventurefuls";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        case 'December':
-            cookieData.name="Raspberry Rallys";
-            cookieData.image="raspberryrally";
-            cookieData.description="Feeling adventurous? Try this month's signature cookie...";
-            break;
-        default:
-            cookieData.name="No cookie";
-            cookieData.image="adventurefuls";
-            cookieData.description="no cookie right now";
-            break;
-    }
-    return cookieData;
+const getMonthlyCookieData = async () => {
+    const response = await fetch('./assets/js/components/promoBanner/cookie-month.json');
+    return response.json();
+}
+const cookieData = await getMonthlyCookieData();
 
+const chooseCookieByMonth = () => {
+    const month = new Date().getMonth();
+    const monthlyCookie = {
+        name: cookieData[month].name,
+        image: cookieData[month].image,
+        description: cookieData[month].description
+    }
+    return monthlyCookie;
 }
 
 class PromoBanner extends HTMLElement {
